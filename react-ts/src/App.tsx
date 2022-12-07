05a9bce4cfb0a0f5a7d069854db559413c51e27b
Priscila-Uchoa <priscilauchoaa@gmail.com>
2022-12-07 20:23:38 -0300

    create generic type

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,51 @@
+import { func } from 'prop-types'
 import React, { useState } from 'react'
 
+function sumNumbers( a: number, b: number){
+  return a + b
+}
+
+function List<ItemType>({items, render}:{
+  items: ItemType[];
+  render:(item:ItemType, index:number)=> React.ReactNode}){return(
+    <ul>
+      {items.map((item, index)=>{
+        return render(item, index)
+      })}
+    </ul>
+  )
+}
+
+const items = [
+  { 
+    id: 1,
+    name: "Wandinha"
+  },
+  {
+    id: 2,
+    name: "Wednesday" 
+  }
+]
+
+const user = {
+  id: 1, 
+  name: "John Doe",
+  age: 33,
+  isAdmin: false,
+}
+
+type UserAttributes = typeof user
+
+const newUser: UserAttributes = {
+  id: 2,
+  name: "Priscila",
+  age: 29,
+  isAdmin: true
+}
+console.log(newUser.name)
+
 type TitleProps = {
-  text: string;
+  text: string
   size?: 'small' | 'large'
 }
 
@@ -9,9 +53,12 @@ type reactNodeType = {
   children: React.ReactNode
 }
 
-const Title = ({text, size}: TitleProps) =>{
+type StyleProps = {
+  color: string
+}
+const Title = ({text, size, color}: TitleProps & StyleProps) =>{
   return <h1 
-    style={{fontSize: size == "small" ? "1.5rem" : "3rem"}}    
+    style={{fontSize: size == "small" ? "1.5rem" : "3rem", color: color}}    
   >
     {text}
   </h1>
@@ -30,13 +77,25 @@ const ReactNode = ({children}: reactNodeType) =>{
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState<number | null>(null)
+  if(count != null){
+    return<div>{count}</div>
+  }
   return (
     <div className="App">
-      <Title text="A Title here" size="small"/>
+      <Title text="A Title here" size="small" color="Violet"/>
       <Content>A Content Here 🤚</Content>
       <ReactNode><span>A React Node here</span></ReactNode>
+      <List
+        items={items}
+        render={(item, index)=>{
+            if(item.id == 1){
+              return <p>{item.name}</p>
+            }
+            return <h3>{item.name}</h3>
+        }}
+
+      ></List>
     </div>
   )
 }
