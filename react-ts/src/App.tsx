eb3ba0e2182afab77200367d63a27f12acf97957
fludpe <fludpe@hm.local>
2022-12-07 19:26:27 -0300

    first class - create type

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react'
+
+type TitleProps = {
+  text: string;
+}
+
+type reactNodeType = {
+  children: React.ReactNode
+}
+
+const Title = ({text}: TitleProps) =>{
+  return <h1>
+    {text}
+  </h1>
+}
+
+const Content = ({children }: {children: string}) =>{
+  return <h1>
+    {children}
+  </h1>
+}
+
+const ReactNode = ({children}: reactNodeType) =>{
+  return <h1>
+    {children}
+  </h1>
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Title text="A Title here"/>
+      <Content>A Content Here 🤚</Content>
+      <ReactNode><span>A React Node here</span></ReactNode>
     </div>
   )
 }
